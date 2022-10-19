a6507450829ffdabc2813ec8c8837aa0974677c8
wangbo0818 <wangbo0818@made-in-china.com>
2022-10-19 11:46:16 +0800

    构造器注入

---
+++ spring-xml/src/main/java/com/scarecrow/xml/construct/ThingOne.java
@@ -0,0 +1,8 @@
+package com.scarecrow.xml.construct;
+
+public class ThingOne {
+
+    public ThingOne(ThingTwo thingTwo, ThingThree thingThree, String four, String five) {
+        System.out.println(11);
+    }
+}
