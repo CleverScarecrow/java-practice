c5f066f777861124c04be2b9bfad36a0328bed70
wangbo0818 <wangbo0818@made-in-china.com>
2022-10-19 13:50:20 +0800

    构造器注入

--- spring-xml/src/main/java/com/scarecrow/xml/construct/ThingOne.java
+++ spring-xml/src/main/java/com/scarecrow/xml/construct/ThingOne.java
@@ -2,7 +2,17 @@ package com.scarecrow.xml.construct;
 
 public class ThingOne {
 
+    private String test;
+
     public ThingOne(ThingTwo thingTwo, ThingThree thingThree, String four, String five) {
         System.out.println(11);
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
     }
 }
