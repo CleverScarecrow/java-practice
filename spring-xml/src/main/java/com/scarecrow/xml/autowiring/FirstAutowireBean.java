0dbf905f5fbffcaf71c1bcf60593812eb42de6e4
wangbo0818 <wangbo0818@made-in-china.com>
2022-10-26 10:47:02 +0800

    autowiring 自动装配

--- spring-xml/src/main/java/com/scarecrow/xml/autowiring/FirstAutowireBean.java
+++ spring-xml/src/main/java/com/scarecrow/xml/autowiring/FirstAutowireBean.java
@@ -1,5 +1,8 @@
 package com.scarecrow.xml.autowiring;
 
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * @author wangbo
  * @since 2022/10/26 9:24
@@ -8,17 +11,23 @@ public class FirstAutowireBean {
 
     private String desc;
 
+    private List<Integer> list;
+
+    private HashMap<Integer, Integer> map;
+
     private SecondAutowireBean secondAutowireBean;
 
     private ThirdAutowireBean thirdAutowireBean;
 
-//    public FirstAutowireBean() {
-//
-//    }
+    public FirstAutowireBean() {
 
-    public FirstAutowireBean(SecondAutowireBean secondAutowireBean, ThirdAutowireBean thirdAutowireBean) {
+    }
+
+    public FirstAutowireBean(SecondAutowireBean secondAutowireBean, ThirdAutowireBean thirdAutowireBean, List<Integer> list, HashMap<Integer, Integer> map) {
         this.secondAutowireBean = secondAutowireBean;
         this.thirdAutowireBean = thirdAutowireBean;
+        this.list = list;
+        this.map = map;
     }
 
     public String getDesc() {
@@ -27,6 +36,22 @@ public class FirstAutowireBean {
 
     public void setDesc(String desc) {
         this.desc = desc;
+    }
+
+    public List<Integer> getList() {
+        return list;
+    }
+
+    public void setList(List<Integer> list) {
+        this.list = list;
+    }
+
+    public HashMap<Integer, Integer> getMap() {
+        return map;
+    }
+
+    public void setMap(HashMap<Integer, Integer> map) {
+        this.map = map;
     }
 
     public SecondAutowireBean getSecondAutowireBean() {
@@ -49,6 +74,8 @@ public class FirstAutowireBean {
     public String toString() {
         return "FirstAutowireBean{" +
                 "desc='" + desc + '\'' +
+                ", list=" + list +
+                ", map=" + map +
                 ", secondAutowireBean=" + secondAutowireBean +
                 ", thirdAutowireBean=" + thirdAutowireBean +
                 '}';
