4fa49917d5e1238aa0b9e7469212d622ea695c82
wangbo <wangbo>
2022-11-02 23:51:56 +0800

    Bean Scope

---
+++ spring-xml/src/main/java/com/scarecrow/xml/scope/SingletonBean.java
@@ -0,0 +1,14 @@
+package com.scarecrow.xml.scope;
+
+public class SingletonBean {
+
+    private String desc;
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
